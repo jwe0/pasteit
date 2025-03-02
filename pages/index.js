f623f24fa01a9c3a616223a7c3e7f436c55fa828
Jwe0 <joshuawebb2007@proton.me>
2025-03-02 21:44:13 +0000

    I hate vercel

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 
 async function make() {
   const data = document.getElementById('message').value
@@ -14,13 +15,12 @@ async function make() {
   const result = await response.json()
   document.getElementById('url').innerText = "Your link: " + window.location.origin + result.url
 }
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-6">
       <nav className="fixed top-0 left-0 w-full bg-gray-800 shadow-md p-4 flex items-center justify-between">
         <h1 className="text-xl font-bold text-white">PasteIt <span className="text-sm text-gray-400">- Paste text anonymously</span></h1>
-        <link href="/legal"><h1 className="text-l text-white align-right">Legal</h1></link>
+        <Link href="/legal"><h1 className="text-l text-white align-right">Legal</h1></Link>
       </nav>
       
       <div className="mt-20 w-full max-w-2xl flex flex-col items-center">
