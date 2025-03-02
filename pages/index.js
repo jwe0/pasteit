983f047aef1baa6cdecdac34a49fbfc8dbea9dfe
Jwe0 <joshuawebb2007@proton.me>
2025-03-02 21:35:14 +0000

    Updated for vercel

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-6">
       <nav className="fixed top-0 left-0 w-full bg-gray-800 shadow-md p-4 flex items-center justify-between">
         <h1 className="text-xl font-bold text-white">PasteIt <span className="text-sm text-gray-400">- Paste text anonymously</span></h1>
-        <a href="/legal"><h1 className="text-l text-white align-right">Legal</h1></a>
+        <link href="/legal"><h1 className="text-l text-white align-right">Legal</h1></link>
       </nav>
       
       <div className="mt-20 w-full max-w-2xl flex flex-col items-center">
