726558720364201f5655ab8b1a6c314b7ae882fa
Jwe0 <joshuawebb2007@proton.me>
2025-03-05 15:26:17 +0000

    Implemented a basic rate limit system

---
+++ middleware.js
@@ -0,0 +1,41 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const ratelimit = new Map();
+
+export async function middleware(request) {
+    let browser_uuid = request.cookies.get("browser_uuid")?.value;
+    const response = NextResponse.next();
+    const limit = 5;
+    const time = 60 * 1000;
+
+    if (!request.nextUrl.pathname.startsWith("/api")) {
+        return response;
+    }
+
+    if (!browser_uuid) {
+        browser_uuid = crypto.randomUUID();
+        response.cookies.set("browser_uuid", browser_uuid);
+    }
+
+    if (!ratelimit.has(browser_uuid)) {
+        ratelimit.set(browser_uuid, {
+            count: 0,
+            last_reset: Date.now()
+        })
+    }
+
+    const data = ratelimit.get(browser_uuid);
+
+    if (Date.now() - data.last_reset > time) {
+        data.count = 0;
+        data.last_reset = Date.now();
+    }
+
+    if (data.count >= limit) {
+        return NextResponse.json({ message: "Rate limit exceeded" }, { status: 429 });
+    }
+
+    data.count+=1;
+
+    return response
+}
